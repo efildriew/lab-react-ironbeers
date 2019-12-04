9614fc6ddd0b607caae96fa38ee86584adfe9949
Marc Valles <13a911@gmail.com>
2019-12-04 19:20:45 +0100

    routes and go-back-home link done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 
 import Beers from "./Beers";
 import NewBeer from "./NewBeer";
@@ -9,11 +9,26 @@ import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <Beers />
-      <RandomBeer />
-      <NewBeer />
-    </div>
+    <>
+      <Route path="/(beers|random-beer|new-beer)">
+        <Link to="/">
+          <img className="home" src="home.png" alt="home" />
+        </Link>
+      </Route>
+      <div>
+        <Route exact path="/">
+          <Link to="/beers">
+            <Beers />
+          </Link>
+          <Link to="/random-beer">
+            <RandomBeer />
+          </Link>
+          <Link to="/new-beer">
+            <NewBeer />
+          </Link>
+        </Route>
+      </div>
+    </>
   );
 };
 
